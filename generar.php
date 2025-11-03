b2fa840c242ade799fc27d85849030b2f813905b
joshuaqm11 <167342009+joshuaqm11@users.noreply.github.com>
2025-11-03 09:12:33 -0600

    Update generar.php

--- generar.php
+++ generar.php
@@ -1,2 +1,7 @@
+// Realizado por Joshua Quesada y Fabio Oconitrillo
 <?php
+// Genera un hash seguro de la contraseña "1234" usando BCRYPT.
+// Útil para insertar contraseñas en la base de datos
+// Cada ejecución generará un hash distinto debido al salt aleatorio incorporado.
+
 echo password_hash('1234', PASSWORD_BCRYPT);
